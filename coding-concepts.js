646896c5757ab1934681c6c6718fed5646503669
dgohdz <diegohmailbox@gmail.com>
2021-05-10 10:19:08 -0700

    complete assessment

--- coding-concepts.js
+++ coding-concepts.js
@@ -7,45 +7,45 @@
 
 // --------------------1) What will this log?
 
-var cohort = "Bravo 2021"
+// var cohort = "Bravo 2021"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: It's going to log characters as numbers.
+// b) Verify and explain: it gave me the number 10. I though it would count from 1 to 10.
 
 
 // --------------------2) What will this log?
 
-var greeting = "Hello World!"
+// var greeting = "Hello World!"
 // console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Will log "Hello World" 3 times.
+// b) Verify and explain: It logged the 3rd character of "hello" since its index 3
 
 
 // --------------------3) What will this log?
 
-var languages = ["JavaScript", "Ruby", "Python", "C++"]
-var index = 1
+// var languages = ["JavaScript", "Ruby", "Python", "C++"]
+// var index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: log will output "Ruby"
+// b) Verify and explain: Index starts at zero, so Ruby is considered the 2nd character of the index.
 
 
 // --------------------4) What will this log?
 
-var weekendDays = ["saturday", "sunday"]
+// var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: will come back as error
+// b) Verify and explain: The characters "saturday & "sunday" are indexed.
 
 
 // --------------------5) What will this log?
 
-var dataTypes = ["number", "string", "Boolean"]
+// var dataTypes = ["number", "string", "Boolean"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Will return string
+// b) Verify and explain: Returns with "number". Not sure why. Couldn't understand "typeof"
